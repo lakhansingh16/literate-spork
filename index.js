df99f8466d9c6c50b6635bee2c29920d4117b6da
Lakhan Singh Barod <lakhanbarod165@gmail.com>
2023-10-23 16:56:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+async function updateReadme() {
+    const fs = require('fs');
+
+    let readme = " \r\n";
+
+    try {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        readme = `Quote on |${new Date().toUTCString()}| : `;
+
+        if (response.ok) {
+            readme += ` ${data["content"]} - ${data["author"]}`;
+        } else {
+            throw new Error('response not OK');
+        }
+    } catch (e) {
+        readme += `Fetching failed`;
+    }
+
+    readme += "\r\n";
+
+    fs.writeFile('quotes.txt', readme, { 'flag': 'a' }, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+    });
+
+}
+
+updateReadme();
